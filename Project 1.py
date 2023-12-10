4d98ad710f4bbd66a679008289c42d891d9cdab1
Rajive Islam <38835340+rajiveislam@users.noreply.github.com>
2023-12-10 19:53:55 +1100

    This is my test file

---
+++ Project 1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+df1= pd.read_csv('tmdb.movies.csv.gz')
+
+
+# In[4]:
+
+
+df1.head(10)
+
+
+# In[ ]:
+
+
+
+
